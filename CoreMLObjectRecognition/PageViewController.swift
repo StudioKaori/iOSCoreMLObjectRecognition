cc8248a5db3c633724aad7b0b792e8816661030b
Studio Kaori <studiokaori.com@gmail.com>
2022-05-19 16:56:34 +0200

    Add: viewControllerBefore

--- CoreMLObjectRecognition/PageViewController.swift
+++ CoreMLObjectRecognition/PageViewController.swift
@@ -46,4 +46,14 @@ extension PageViewController: UIPageViewControllerDataSource {
     func presentationCount(for pageViewController: UIPageViewController) -> Int {
         return controllers.count
     }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        if let index = controllers.firstIndex(of: viewController),
+           index < controllers.count - 1 {
+            return controllers[index + 1]
+        }
+        return nil
+    }
+    
+    
 }
