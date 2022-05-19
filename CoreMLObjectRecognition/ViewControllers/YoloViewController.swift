672c885da2570f3c92bee4b9c89ed457c49db170
Studio Kaori <studiokaori.com@gmail.com>
2022-05-19 19:52:49 +0200

    Adjust: sorting

--- CoreMLObjectRecognition/ViewControllers/YoloViewController.swift
+++ CoreMLObjectRecognition/ViewControllers/YoloViewController.swift
@@ -6,15 +6,80 @@
 //
 
 import UIKit
+import AVFoundation
+import Vision
 
-class YoloViewController: UIViewController {
+class YoloViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
+    
+    var recognitionInterval = 0
+    
+    var mlModel: VNCoreMLModel?
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
     }
     
+    func setVideo() {
+        let session = AVCaptureSession()
+        
+        // output
+        var deviceInput:AVCaptureDeviceInput
+        let videoDevice = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInWideAngleCamera], mediaType: .video, position: .back).devices.first
+        do {
+            deviceInput = try AVCaptureDeviceInput(device: videoDevice!)
+        } catch {
+            print("Could not create video device input: \(error)")
+            return
+        }
+
+        session.beginConfiguration()
+        session.sessionPreset = .vga640x480 // Model image size is smaller.
+        
+        
+        // input
+        guard session.canAddInput(deviceInput) else {
+            print("Could not add video device input to the session")
+            session.commitConfiguration()
+            return
+        }
+        session.addInput(deviceInput)
+        
+        
+        
+        // add output to the session
+        if session.canAddOutput(videoDataOutput) {
+            session.addOutput(videoDataOutput)
+            // Add a video data output
+            videoDataOutput.alwaysDiscardsLateVideoFrames = true
+            videoDataOutput.videoSettings = [kCVPixelBufferPixelFormatTypeKey as String: Int(kCVPixelFormatType_420YpCbCr8BiPlanarFullRange)]
+            videoDataOutput.setSampleBufferDelegate(self, queue: videoDataOutputQueue)
+        } else {
+            print("Could not add video data output to the session")
+            session.commitConfiguration()
+            return
+        }
+        
+        
+        let captureConnection = videoDataOutput.connection(with: .video)
+        // Always process the frames
+        captureConnection?.isEnabled = true
+        do {
+            try  videoDevice!.lockForConfiguration()
+            let dimensions = CMVideoFormatDescriptionGetDimensions((videoDevice?.activeFormat.formatDescription)!)
+            bufferSize.width = CGFloat(dimensions.width)
+            bufferSize.height = CGFloat(dimensions.height)
+            videoDevice!.unlockForConfiguration()
+        } catch {
+            print(error)
+        }
+        
+        session.commitConfiguration()
+        
+        
+        
+    }
 
     /*
     // MARK: - Navigation
