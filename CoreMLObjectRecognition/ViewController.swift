208dc80af7226d23b95bf59b14ad2746ae7afb0e
Studio Kaori <studiokaori.com@gmail.com>
2022-05-19 13:59:26 +0200

    Add: setModel

--- CoreMLObjectRecognition/ViewController.swift
+++ CoreMLObjectRecognition/ViewController.swift
@@ -24,6 +24,7 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
         
         // Initial setting for video
         setVideo()
+        setModel()
     }
 
     func setVideo() {
