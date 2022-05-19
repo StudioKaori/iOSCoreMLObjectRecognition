5834c89fa9384936e5d948001adddcb4d9de1c02
Studio Kaori <studiokaori.com@gmail.com>
2022-05-19 16:33:51 +0200

    Add: PageViewController

---
+++ CoreMLObjectRecognition/PageViewController.swift
@@ -0,0 +1,31 @@
+//
+//  PageViewController.swift
+//  CoreMLObjectRecognition
+//
+//  Created by Kaori Persson on 2022-05-19.
+//
+
+import UIKit
+
+class PageViewController: UIPageViewController {
+    
+    private var controllers = [UIViewController]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupPageViewController()
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
